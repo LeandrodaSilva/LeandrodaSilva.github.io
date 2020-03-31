0923f1faf9574e8eb45bc64f020b076ce002f4ff
LeandrodaSilva <ld_silva13@hotmail.com>
2020-03-30 22:25:46 -0300

    0.0.0

---
+++ precache-manifest.03c0befbfda79ac076dc394da78cd593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51bd7da00d785cc223e9a19909119f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfdc60d0801a20a2084c",
+    "url": "/static/css/main.d5242448.chunk.css"
+  },
+  {
+    "revision": "47de86d797a1589309c5",
+    "url": "/static/js/2.b30a7c25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b30a7c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfdc60d0801a20a2084c",
+    "url": "/static/js/main.943dac6c.chunk.js"
+  },
+  {
+    "revision": "64a55cdf2044bec9b19d",
+    "url": "/static/js/runtime-main.a30896b4.js"
+  }
+]);
